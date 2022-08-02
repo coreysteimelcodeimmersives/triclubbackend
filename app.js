255e4169a6a1da18adf5bdfb47be5de735a86106
Corey Steimel <coreysteimel@Coreys-MacBook-Pro-Work.local>
2022-08-02 10:52:43 -0600

    require dotenv

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var authRouter = require("./routes/auth");
 
+require("dotenv").config();
 var app = express();
 
 var { mongoConnect } = require("./mongo.js");
