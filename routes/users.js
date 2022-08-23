f94b7b5b3946d2b85aa68c344797ff27c1e987c4
Corey Steimel <coreysteimel@Coreys-MacBook-Pro-Work.local>
2022-08-22 20:19:32 -0600

    purchase flow

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 const dotenv = require("dotenv");
+const jwt = require("jsonwebtoken");
 const { triclubDb } = require("../mongo");
 const { env } = require("process");
 
@@ -82,16 +83,147 @@ router.put("/set-pending-coaches", async (req, res) => {
       }
     );
 
-    res
-      .status(200)
-      .json({
-        success: true,
-        message:
-          "Coach successfully accepted and assigned 'Coach' access priveledges",
-      });
+    res.status(200).json({
+      success: true,
+      message:
+        "Coach successfully accepted and assigned 'Coach' access priveledges",
+    });
   } catch (error) {
     return res.status(500).json({ success: false, message: String(error) });
   }
 });
 
-module.exports = router;
+router.put("/purchase-program", async (req, res) => {
+  try {
+    //VERIFY ALL THE USER INPUT
+    const userInfo = req.body.userInfo;
+    const programId = req.body.programId;
+    const token = req.headers.token;
+    const jwtSecretKey = process.env.REACT_APP_JWT_SECRET_KEY;
+    if (!token) {
+      return res
+        .status(500)
+        .json({ success: false, message: "no jwt token exists" });
+    }
+    const verified = jwt.verify(token, jwtSecretKey);
+    const userObj = await findUserByKey("uid", verified.userId);
+    if (!userObj.success) {
+      res.status(406).json({
+        success: false,
+        message: "We did not find your account.",
+      });
+      return;
+    }
+    const user = userObj.user;
+    let userChildrenArr = user.children;
+    const child = userInfo.child;
+    if (
+      !userChildrenArr.some(
+        (someChild) => someChild.firstNameChild === child.firstNameChild
+      )
+    ) {
+      userChildrenArr = [...userChildrenArr, child];
+    }
+    let userPrograms = JSON.parse(JSON.stringify(user.progams));
+    console.log("user info", userPrograms);
+    const newPrograms = {
+      ...userPrograms,
+      [programId]: userInfo.child.firstNameChild,
+    };
+    console.log("new user programs ", newPrograms);
+    const collection = await triclubDb().collection("users");
+    await collection.updateOne(
+      { uid: verified.userId },
+      {
+        $set: {
+          ...user,
+          userInfo: {
+            addressOne: userInfo.addressOne,
+            addressTwo: userInfo.addressTwo,
+            city: userInfo.city,
+            firstName: userInfo.firstName,
+            lastName: userInfo.lastName,
+            stateAbb: userInfo.stateAbb,
+            telephone: userInfo.telephone,
+            zipCode: userInfo.zipCode,
+          },
+          children: userChildrenArr,
+          progams: {
+            ...newPrograms,
+          },
+        },
+      }
+    );
+    return res.status(200).json({
+      success: true,
+      message: "User purchased program",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ success: false, message: String(error) });
+  }
+});
+
+router.get("/get-user-info", async (req, res) => {
+  try {
+    const token = req.headers.token;
+    const jwtSecretKey = process.env.REACT_APP_JWT_SECRET_KEY;
+    if (!token) {
+      return res
+        .status(500)
+        .json({ success: false, message: "no jwt token exists" });
+    }
+    const verified = jwt.verify(token, jwtSecretKey);
+    const userObj = await findUserByKey("uid", verified.userId);
+    if (!userObj.success) {
+      res.status(406).json({
+        success: false,
+        message: "We did not find your account.",
+      });
+      return;
+    }
+    const user = userObj.user;
+    const userInfo = user.userInfo;
+    return res.status(200).json({
+      success: true,
+      message: "you should have the user info",
+      obj: userInfo,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ success: false, message: String(error) });
+  }
+});
+
+router.get("/get-user-children", async (req, res) => {
+  try {
+    const token = req.headers.token;
+    const jwtSecretKey = process.env.REACT_APP_JWT_SECRET_KEY;
+    if (!token) {
+      return res
+        .status(500)
+        .json({ success: false, message: "no jwt token exists" });
+    }
+    const verified = jwt.verify(token, jwtSecretKey);
+    const userObj = await findUserByKey("uid", verified.userId);
+    if (!userObj.success) {
+      res.status(406).json({
+        success: false,
+        message: "We did not find your account.",
+      });
+      return;
+    }
+    const user = userObj.user;
+    const children = user.children;
+    return res.status(200).json({
+      success: true,
+      message: "you should have the children array",
+      obj: children,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ success: false, message: String(error) });
+  }
+});
+
+(module.exports = router), findUserByKey;
